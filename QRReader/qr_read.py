c8d15eeb18af6291264578f206cf165ac29fe60e
dotaky99 <67868070+dotaky99@users.noreply.github.com>
2022-07-13 23:05:13 +0900

    Update qr_read.py

--- QRReader/qr_read.py
+++ QRReader/qr_read.py
@@ -10,3 +10,15 @@ num = 0
 for i in file_list:
     print(file_list[num] + " : " + decode(Image.open("qr/"+i))[0].data.decode())
     num += 1
+
+
+from PIL import Image
+from pyzbar.pyzbar import decode
+import os
+
+
+dir = 'directory'
+file_list = os.listdir(dir)
+
+for img in file_list:
+  print (decode(Image.open('dir/' + img))[0].data.decode())
