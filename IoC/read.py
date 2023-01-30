7990a94c6b05de58b6d96d7ceb4cea4346a409c5
dotaky99 <dotaky99@gmail.com>
2023-01-30 22:58:44 +0900

    edit

--- IoC/read.py
+++ IoC/read.py
@@ -1,55 +1,44 @@
-import os, datetime, hashlib, sys
-
-# Read the path.txt
-all_files = []
-with open("path.txt","r") as f:
-    temp = f.read().splitlines()
-
-for file in temp:
-    all_files.append(file.replace('\\', '/'))
-
-print(all_files)
-
-for file in all_files:
-    print(os.path.getctime(file))
-    create_time = os.path.getctime(file)
-    modify_time = os.path.getmtime(file)
-    access_time = os.path.getatime(file)
-    size = os.path.getsize(file)
-
-    with open(file, "rb") as f:
-        hash_data = f.read()
-    
-    md5 = hashlib.md5(hash_data).hexdigest()
-    sha1 = hashlib.sha1(hash_data).hexdigest()
-    owner = os.stat(file).st_uid
-    print(file)
-    print(create_time, modify_time, access_time, size, owner)
-    print(md5, sha1)
-    print(owner)
-    print()
-    print(os.stat(file))
-
-    strings = str(file)
-    strings += str(create_time)
-    strings += str(modify_time)
-    strings += str(access_time)
-    strings += str(size)
-    
-    break
-
-def GetFileInfo(file):
-    # os.stat_result(st_mode=33206, st_ino=562949953932938, st_dev=305986030, st_nlink=1, st_uid=0, st_gid=0, st_size=1221, st_atime=1675084201, st_mtime=1675084200, st_ctime=1675066108)
-    stat_information = os.stat(file)
-    return stat_information
+import os, datetime, hashlib
 
 def GetFileHash(file):
     with open(file, "rb") as f:
         hash_data = f.read()
-    
+
     md5 = hashlib.md5(hash_data).hexdigest()
     sha1 = hashlib.sha1(hash_data).hexdigest()
 
+    return md5, sha1
+
+def GetFileInfo(all_files):
+    # st_mode, st_ino, st_dev, st_nlink, st_uid, st_gid, st_size, st_atime, st_mtime, st_ctime
+    print(all_files)
+    f = open("result.txt", "w")
+    for file in all_files:
+        stat_info = os.stat(file)
+
+        strings = file + "\t"
+        strings += str(datetime.datetime.fromtimestamp(stat_info.st_ctime)) + "\t"
+        strings += str(datetime.datetime.fromtimestamp(stat_info.st_mtime)) + "\t"
+        strings += str(datetime.datetime.fromtimestamp(stat_info.st_atime)) + "\t"
+        get_md5, get_sha1 = GetFileHash(file)
+        strings += str(get_md5) + "\t"
+        strings += str(get_sha1) + "\t"
+        strings += str(stat_info.st_size) + "\t"
+        strings += str(stat_info.st_uid) + "\t"
+        strings += "\n"
+        
+        print(strings)
+        
+        f.writelines(strings)
+    f.close()
+
 if __name__ == "__main__":
-    GetFileInfo()
-    GetFileHash()
+
+    all_files = []
+    with open("path.txt","r") as f:
+        temp = f.read().splitlines()
+
+    for file in temp:
+        all_files.append(file.replace('\\', '/'))
+
+    GetFileInfo(all_files)
